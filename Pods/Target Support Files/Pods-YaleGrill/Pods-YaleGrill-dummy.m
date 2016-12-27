26104b489cb54ade56481927423f308c5e34b399
philvasseur <pvass153@gmail.com>
2016-12-27 16:38:54 -0500

    Initial Commit

---
+++ Pods/Target Support Files/Pods-YaleGrill/Pods-YaleGrill-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YaleGrill : NSObject
+@end
+@implementation PodsDummy_Pods_YaleGrill
+@end
---
+++ YaleGrill/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YaleGrill
+//
+//  Created by Phil Vasseur on 12/27/16.
+//  Copyright © 2016 Phil Vasseur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, GIDSignInUIDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GIDSignIn.sharedInstance().uiDelegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
